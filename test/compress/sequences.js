45a3970ca35e0c640a761601d0854a4c7c5a5cf0
Mihai Bazon <mihai@bazon.net>
2012-09-17 12:53:52 +0300

    one more test for sequences

--- test/compress/sequences.js
+++ test/compress/sequences.js
@@ -58,3 +58,32 @@ make_sequences_3: {
         }
     }
 }
+
+make_sequences_4: {
+    options = {
+        sequences: true
+    };
+    input: {
+        x = 5;
+        if (y) z();
+
+        x = 5;
+        for (i = 0; i < 5; i++) console.log(i);
+
+        x = 5;
+        for (; i < 5; i++) console.log(i);
+
+        x = 5;
+        switch (y) {}
+
+        x = 5;
+        with (obj) {}
+    }
+    expect: {
+        if (x = 5, y) z();
+        for (x = 5, i = 0; i < 5; i++) console.log(i);
+        for (x = 5; i < 5; i++) console.log(i);
+        switch (x = 5, y) {}
+        with (x = 5, obj) {}
+    }
+}
