bb3eb65015666fc1e9de1f91b9c1589f2346d5ad
David Glasser <glasser@davidglasser.net>
2013-01-16 14:59:19 -0500

    Fix output for arrays whose last element is a hole: [1,,]
    1529ab96 started to do this (by considering holes to be separate from
    "undefined") but it still converted
       [1,,]    (length 2, last element hole, trailing comma)
    to
       [1,]     (length 1, trailing comma)
    
    Unfortunately the test suite doesn't really make this clear: the new test here
    passes with or without this patch because run-tests.js beautifys the expected
    output (in "make_code"), which does the incorrect transformation! If you make
    some manual change to arrays.js to make the test fail and see the INPUT and
    OUTPUT, then you can see that without this fix, [1,,] -> [1,], and with this fix
    it stays [1,,].

--- test/compress/arrays.js
+++ test/compress/arrays.js
@@ -1,10 +1,12 @@
 holes_and_undefined: {
     input: {
+        w = [1,,];
         x = [1, 2, undefined];
         y = [1, , 2, ];
         z = [1, undefined, 3];
     }
     expect: {
+        w=[1,,];
         x=[1,2,void 0];
         y=[1,,2];
         z=[1,void 0,3];
