78e98d2611f5698797785adbd12faf2ba46ec783
Mihai Bazon <mihai.bazon@gmail.com>
2013-09-19 18:20:45 +0300

    When `unsafe` is set, evaluate [...].join() if possible
    Close #298

--- test/compress/arrays.js
+++ test/compress/arrays.js
@@ -12,3 +12,22 @@ holes_and_undefined: {
         z=[1,void 0,3];
     }
 }
+
+constant_join: {
+    options = {
+        unsafe   : true,
+        evaluate : true
+    };
+    input: {
+        var a = [ "foo", "bar", "baz" ].join("");
+        var b = [ "foo", 1, 2, 3, "bar" ].join("");
+        var c = [ boo(), "foo", 1, 2, 3, "bar", bar() ].join("");
+        var d = [ "foo", 1 + 2 + "bar", "baz" ].join("-");
+    }
+    expect: {
+        var a = "foobarbaz";
+        var b = "foo123bar";
+        var c = [ boo(), "foo", 1, 2, 3, "bar", bar() ].join(""); // we could still shorten this one, but oh well.
+        var d = "foo-3bar-baz";
+    }
+}
