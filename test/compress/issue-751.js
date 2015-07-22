905b6011784ca60d41919ac1a499962b7c1d4b02
Mihai Bazon <mihai.bazon@gmail.com>
2015-07-22 16:53:25 +0300

    Don't attempt to negate non-boolean AST_Binary
    Fix #751

---
+++ test/compress/issue-751.js
@@ -0,0 +1,29 @@
+negate_booleans_1: {
+    options = {
+        comparisons: true
+    };
+    input: {
+        var a = !a || !b || !c || !d || !e || !f;
+    }
+    expect: {
+        var a = !(a && b && c && d && e && f);
+    }
+}
+
+negate_booleans_2: {
+    options = {
+        comparisons: true
+    };
+    input: {
+        var match = !x &&       // should not touch this one
+            (!z || c) &&
+            (!k || d) &&
+            the_stuff();
+    }
+    expect: {
+        var match = !x &&
+            (!z || c) &&
+            (!k || d) &&
+            the_stuff();
+    }
+}
