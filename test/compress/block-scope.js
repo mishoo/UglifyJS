3d7f73114d52acbba9f482bdd289986253ba2ec0
Fábio Santos <fabiosantosart@gmail.com>
2015-09-06 21:56:55 +0100

    Add a test to make sure future generations don't hoist lets

--- test/compress/block-scope.js
+++ test/compress/block-scope.js
@@ -6,3 +6,28 @@ let_statement: {
     expect_exact: "let x=6;"
 }
 
+do_not_hoist_let: {
+    options = {
+        hoist_vars: true,
+    };
+    input: {
+        function x() {
+            if (FOO) {
+                let let1;
+                let let2;
+                var var1;
+                var var2;
+            }
+        }
+    }
+    expect: {
+        function x() {
+            var var1, var2;
+            if (FOO) {
+                let let1;
+                let let2;
+            }
+        }
+    }
+}
+
