f97da4294a7e9adbd560ecafd94ec697de35affc
Samuel Reed <samuel.trace.reed@gmail.com>
2016-01-20 10:52:48 -0600

    Use TreeWalker for more accurate @const results and update tests

--- test/compress/dead-code.js
+++ test/compress/dead-code.js
@@ -97,6 +97,7 @@ dead_code_const_declaration: {
         evaluate     : true
     };
     input: {
+        var unused;
         const CONST_FOO = false;
         if (CONST_FOO) {
             console.log("unreachable");
@@ -105,6 +106,7 @@ dead_code_const_declaration: {
         }
     }
     expect: {
+        var unused;
         const CONST_FOO = !1;
         var moo;
         function bar() {}
@@ -120,7 +122,8 @@ dead_code_const_annotation: {
         evaluate     : true
     };
     input: {
-        /** @const*/ var CONST_FOO_ANN = false;
+        var unused;
+        /** @const */ var CONST_FOO_ANN = false;
         if (CONST_FOO_ANN) {
             console.log("unreachable");
             var moo;
@@ -128,8 +131,52 @@ dead_code_const_annotation: {
         }
     }
     expect: {
+        var unused;
         var CONST_FOO_ANN = !1;
         var moo;
         function bar() {}
+    }
+}
+
+dead_code_const_annotation_complex_scope: {
+    options = {
+        dead_code    : true,
+        loops        : true,
+        booleans     : true,
+        conditionals : true,
+        evaluate     : true
+    };
+    input: {
+        var unused_var;
+        /** @const */ var test = 'test';
+        /** @const */ var CONST_FOO_ANN = false;
+        var unused_var_2;
+        if (CONST_FOO_ANN) {
+            console.log("unreachable");
+            var moo;
+            function bar() {}
+        }
+        if (test === 'test') {
+            var beef = 'good';
+            /** @const */ var meat = 'beef';
+            var pork = 'bad';
+            if (meat === 'pork') {
+                console.log('also unreachable');
+            } else if (pork === 'good') {
+                console.log('reached, not const');
+            }
+        }
+    }
+    expect: {
+        var unused_var;
+        var test = 'test';
+        var CONST_FOO_ANN = !1;
+        var unused_var_2;
+        var moo;
+        function bar() {}
+        var beef = 'good';
+        var meat = 'beef';
+        var pork = 'bad';
+        'good' === pork && console.log('reached, not const');
     }
 }
