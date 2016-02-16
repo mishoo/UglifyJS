2e354a3872688a244ea3f12f3b4b4269fc5c6679
alexlamsl <alexlamsl@gmail.com>
2016-02-17 02:52:28 +0800

    preserve ThisBinding for side_effects

--- test/compress/issue-782.js
+++ test/compress/issue-782.js
@@ -1,10 +1,12 @@
 remove_redundant_sequence_items: {
     options = { side_effects: true };
     input: {
+        (0, 1, eval)();
         (0, 1, logThis)();
         (0, 1, _decorators.logThis)();
     }
     expect: {
+        (0, eval)();
         logThis();
         (0, _decorators.logThis)();
     }
@@ -13,10 +15,12 @@ remove_redundant_sequence_items: {
 dont_remove_this_binding_sequence: {
     options = { side_effects: true };
     input: {
+        (0, eval)();
         (0, logThis)();
         (0, _decorators.logThis)();
     }
     expect: {
+        (0, eval)();
         logThis();
         (0, _decorators.logThis)();
     }
