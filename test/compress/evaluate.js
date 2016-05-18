bc49dfd27a800cfa2070464c236a1d56ed30bfca
Richard van Velzen <rvanvelzen@experty.com>
2016-05-18 18:49:55 +0200

    Completely allow evaluating -0

--- test/compress/evaluate.js
+++ test/compress/evaluate.js
@@ -1,9 +1,39 @@
 negative_zero: {
     options = { evaluate: true }
     input: {
-        assert.sameValue(-"", -0, '-""');
+        console.log(
+            -"",
+            - -"",
+            1 / (-0),
+            1 / (-"")
+        );
     }
     expect: {
-        assert.sameValue(-"", -0, '-""');
+        console.log(
+            -0,
+            0,
+            1 / (-0),
+            1 / (-0)
+        );
+    }
+}
+
+positive_zero: {
+    options = { evaluate: true }
+    input: {
+        console.log(
+            +"",
+            + -"",
+            1 / (+0),
+            1 / (+"")
+        );
+    }
+    expect: {
+        console.log(
+            0,
+            -0,
+            1 / (0),
+            1 / (0)
+        );
     }
 }
