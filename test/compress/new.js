5d856bf8a0e3ac07dd2aca1ea818935d9bfde898
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2016-06-07 01:33:13 +0200

    Fix uglify attempting to rewrite invalid new expressions

--- test/compress/new.js
+++ test/compress/new.js
@@ -10,3 +10,11 @@ new_statement: {
     }
     expect_exact: "new x(1);new x(1)(2);new x(1)(2)(3);new new x(1);new new x(1)(2);new new x(1)(2);(new new x(1))(2);"
 }
+
+new_with_rewritten_true_value: {
+    options = { booleans: true }
+    input: {
+        new true;
+    }
+    expect_exact: "new(!0);"
+}
