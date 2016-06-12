5c4cfaa0a75317582c979f9b50c1e562fbcfa40d
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2016-06-12 17:34:05 +0200

    Re-add parens after new expression in beautify mode

--- test/mocha/new.js
+++ test/mocha/new.js
@@ -2,7 +2,49 @@ var assert = require("assert");
 var uglify = require("../../");
 
 describe("New", function() {
-    it("Should attach callback parens after some tokens", function() {
+    it("Should add trailing parentheses for new expressions with zero arguments in beautify mode", function() {
+        var tests = [
+            "new x(1);",
+            "new x;",
+            "new new x;",
+            "new (function(foo){this.foo=foo;})(1);",
+            "new (function(foo){this.foo=foo;})();",
+            "new (function test(foo){this.foo=foo;})(1);",
+            "new (function test(foo){this.foo=foo;})();",
+            "new true;",
+            "new (0);",
+            "new (!0);",
+            "new (bar = function(foo) {this.foo=foo;})(123);",
+            "new (bar = function(foo) {this.foo=foo;})();"
+        ];
+        var expected = [
+            "new x(1);",
+            "new x();",
+            "new new x()();",
+            "new function(foo) {\n    this.foo = foo;\n}(1);",
+            "new function(foo) {\n    this.foo = foo;\n}();",
+            "new function test(foo) {\n    this.foo = foo;\n}(1);",
+            "new function test(foo) {\n    this.foo = foo;\n}();",
+            "new true();",
+            "new 0();",
+            "new (!0)();",
+            "new (bar = function(foo) {\n    this.foo = foo;\n})(123);",
+            "new (bar = function(foo) {\n    this.foo = foo;\n})();"
+        ];
+        for (var i = 0; i < tests.length; i++) {
+            assert.strictEqual(
+                uglify.minify(tests[i], {
+                    fromString: true,
+                    output: {beautify: true},
+                    compress: false,
+                    mangle: false
+                }).code,
+                expected[i]
+            );
+        }
+    });
+
+    it("Should not add trailing parentheses for new expressions with zero arguments in non-beautify mode", function() {
         var tests = [
             "new x(1);",
             "new x;",
@@ -20,22 +62,22 @@ describe("New", function() {
         var expected = [
             "new x(1);",
             "new x;",
-            "new (new x);",
-            "new function(foo) {\n    this.foo = foo;\n}(1);",
-            "new function(foo) {\n    this.foo = foo;\n};",
-            "new function test(foo) {\n    this.foo = foo;\n}(1);",
-            "new function test(foo) {\n    this.foo = foo;\n};",
+            "new(new x);",
+            "new function(foo){this.foo=foo}(1);",
+            "new function(foo){this.foo=foo};",
+            "new function test(foo){this.foo=foo}(1);",
+            "new function test(foo){this.foo=foo};",
             "new true;",
             "new 0;",
-            "new (!0);",
-            "new (bar = function(foo) {\n    this.foo = foo;\n})(123);",
-            "new (bar = function(foo) {\n    this.foo = foo;\n});"
+            "new(!0);",
+            "new(bar=function(foo){this.foo=foo})(123);",
+            "new(bar=function(foo){this.foo=foo});"
         ];
         for (var i = 0; i < tests.length; i++) {
             assert.strictEqual(
                 uglify.minify(tests[i], {
                     fromString: true,
-                    output: {beautify: true},
+                    output: {beautify: false},
                     compress: false,
                     mangle: false
                 }).code,
