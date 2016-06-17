6eaeb19a4a5ecbb890fc025591bb24d466d3efe0
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2016-06-18 00:25:18 +0200

    Add exponentiation operator

---
+++ test/mocha/expression.js
@@ -0,0 +1,32 @@
+var assert = require("assert");
+var uglify = require("../../");
+
+describe("Expression", function() {
+    it("Should not allow the first exponentiation operator to be prefixed with an unary operator", function() {
+        var tests = [
+            "+5 ** 3",
+            "-5 ** 3",
+            "~5 ** 3",
+            "!5 ** 3",
+            "void 5 ** 3",
+            "typeof 5 ** 3",
+            "delete 5 ** 3",
+            "var a = -(5) ** 3;"
+        ];
+
+        var fail = function(e) {
+            return e instanceof uglify.JS_Parse_Error &&
+                /^SyntaxError: Unexpected token: operator \((?:[!+~-]|void|typeof|delete)\)/.test(e.message);
+        }
+
+        var exec = function(test) {
+            return function() {
+                uglify.parse(test);
+            }
+        }
+
+        for (var i = 0; i < tests.length; i++) {
+            assert.throws(exec(tests[i]), fail, tests[i]);
+        }
+    });
+});
