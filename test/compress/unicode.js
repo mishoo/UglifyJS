54a783ba84892346213f61ccf524c3a48a7cb6e9
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2016-06-26 19:44:22 +0200

    Add ecma5 flag for codegen

--- test/compress/unicode.js
+++ test/compress/unicode.js
@@ -17,6 +17,7 @@ unicode_parse_variables: {
 }
 
 unicode_escaped_identifier: {
+    beautify = {ecma: 6}
     input: {
         var \u{61} = "foo";
         var \u{10000} = "bar";
@@ -25,7 +26,7 @@ unicode_escaped_identifier: {
 }
 
 unicode_identifier_ascii_only: {
-    beautify = {ascii_only: true}
+    beautify = {ascii_only: true, ecma: 6}
     input: {
         var \u{0061} = "hi";
         var bar = "h\u{0065}llo";
@@ -35,9 +36,67 @@ unicode_identifier_ascii_only: {
 }
 
 unicode_string_literals: {
-    beautify = {ascii_only: true}
+    beautify = {ascii_only: true, ecma: 6}
     input: {
         var a = "6 length unicode character: \u{101111}";
     }
     expect_exact: 'var a="6 length unicode character: \\u{101111}";'
+}
+
+unicode_output_es5_surrogates: {
+    beautify = {ascii_only: true, ecma: 5}
+    input: {
+        var \u{10000} = "6 length unicode character: \u{10FFFF}";
+    }
+    expect_exact: 'var \\ud800\\udc00="6 length unicode character: \\udbff\\udfff";'
+}
+
+check_escape_style: {
+    beautify = {ascii_only: true, ecma: 6}
+    input: {
+        var a = "\x01";
+        var \ua0081 = "\x10"; // \u0081 only in ID_Continue
+        var \u0100 = "\u0100";
+        var \u1000 = "\u1000";
+        var \u{10000} = "\u{10000}";
+        var \u{2f800} = "\u{100000}";
+    }
+    expect_exact: 'var a="\\x01";var \\ua0081="\\x10";var \\u0100="\\u0100";var \\u1000="\\u1000";var \\u{10000}="\\u{10000}";var \\u{2f800}="\\u{100000}";'
+}
+
+check_escape_style_es5: {
+    beautify = {ascii_only: true, ecma: 5}
+    input: {
+        var a = "\x01";
+        var \ua0081 = "\x10"; // \u0081 only in ID_Continue
+        var \u0100 = "\u0100";
+        var \u1000 = "\u1000";
+        var \u{10000} = "\u{10000}";
+        var \u{2f800} = "\u{100000}";
+    }
+    expect_exact: 'var a="\\x01";var \\ua0081="\\x10";var \\u0100="\\u0100";var \\u1000="\\u1000";var \\ud800\\udc00="\\ud800\\udc00";var \\ud87e\\udc00="\\udbc0\\udc00";'
+}
+
+ID_continue_with_surrogate_pair: {
+    beautify = {ascii_only: true, ecma: 6}
+    input: {
+        var \u{2f800}\u{2f800}\u{2f800}\u{2f800} = "\u{100000}\u{100000}\u{100000}\u{100000}\u{100000}";
+    }
+    expect_exact: 'var \\u{2f800}\\u{2f800}\\u{2f800}\\u{2f800}="\\u{100000}\\u{100000}\\u{100000}\\u{100000}\\u{100000}";'
+}
+
+escape_non_escaped_identifier: {
+    beautify = {ascii_only: true, ecma: 6}
+    input: {
+        var µþ = "µþ";
+    }
+    expect_exact: 'var \\u00b5\\u00fe="\\xb5\\xfe";'
+}
+
+non_escape_2_non_escape: {
+    beautify = {ascii_only: false, ecma: 6}
+    input: {
+        var µþ = "µþ";
+    }
+    expect_exact: 'var µþ="µþ";'
 }
