3f8fc3a316a60b67acf09b2b2cf887f0209c7d71
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2016-07-21 18:02:32 +0200

    Fix computed getters + cleanup AST

--- test/compress/object.js
+++ test/compress/object.js
@@ -240,24 +240,5 @@ getter_setter_with_computed_value: {
             }
         }
     }
-    expect: {
-        class C {
-            get ['a']() {
-                return 'A';
-            }
-            set ['a'](value) {
-                do_something(a);
-            }
-        }
-        var x = {
-            get [a.b]() {
-                return 42;
-            }
-        };
-        class MyArray extends Array {
-            get [Symbol.species]() {
-                return Array;
-            }
-        }
-    }
+    expect_exact: 'class C{get["a"](){return"A"}set["a"](value){do_something(a)}}var x={get[a.b](){return 42}};class MyArray extends Array{get[Symbol.species](){return Array}}'
 }
