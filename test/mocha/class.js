f1068d26dd21830db8a3e751bf9bcaec5bd43611
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2016-07-29 03:18:21 +0200

    Fix quoting of properties
    - Make AST_ConciseMethod child of AST_ObjectProperty.
    - Fix some typos.

--- test/mocha/class.js
+++ test/mocha/class.js
@@ -23,4 +23,35 @@ describe("Class", function() {
             assert.throws(test(tests[i]), error);
         }
     });
+
+    it("Should return the correct token for class methods", function() {
+        var tests = [
+            {
+                code: "class foo{static test(){}}",
+                token_value_start: "static",
+                token_value_end: "}"
+            },
+            {
+                code: "class bar{*procedural(){}}",
+                token_value_start: "*",
+                token_value_end: "}"
+            },
+            {
+                code: "class foobar{aMethod(){}}",
+                token_value_start: "aMethod",
+                token_value_end: "}"
+            },
+            {
+                code: "class foobaz{get something(){}}",
+                token_value_start: "get",
+                token_value_end: "}"
+            }
+        ];
+
+        for (var i = 0; i < tests.length; i++) {
+            var ast = uglify.parse(tests[i].code);
+            assert.strictEqual(ast.body[0].properties[0].start.value, tests[i].token_value_start);
+            assert.strictEqual(ast.body[0].properties[0].end.value, tests[i].token_value_end);
+        }
+    });
 });
