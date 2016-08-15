7fa0dbdeb97d01eb40cb2ab492531eac3c0cd820
Richard van Velzen <rvanvelzen@experty.com>
2016-08-15 09:54:15 +0200

    Ignore default reserved properties in compress test

--- test/compress/properties.js
+++ test/compress/properties.js
@@ -99,7 +99,8 @@ mangle_unquoted_properties: {
         properties: false
     }
     mangle_props = {
-        ignore_quoted: true
+        ignore_quoted: true,
+        reserved: []
     }
     beautify = {
         beautify: false,
