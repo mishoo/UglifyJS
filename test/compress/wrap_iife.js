6389e52305f0d2e75a7bc6f8703c8d163d04cb99
Richard van Velzen <rvanvelzen@experty.com>
2016-10-06 14:11:19 +0200

    Remove console.log and add extra test case

--- test/compress/wrap_iife.js
+++ test/compress/wrap_iife.js
@@ -15,6 +15,21 @@ wrap_iife: {
     expect_exact: '(function(){return function(){console.log("test")}})()();'
 }
 
+wrap_iife_in_expression: {
+    options = {
+        negate_iife: false,
+    }
+    beautify = {
+        wrap_iife: true,
+    }
+    input: {
+        foo = (function () {
+            return bar();
+        })();
+    }
+    expect_exact: 'foo=(function(){return bar()})();'
+}
+
 wrap_iife_in_return_call: {
     options = {
         negate_iife: false,
