1eaa211e0932105439d98d4f03a981f157f0a77c
Alex Lam S.L <alexlamsl@gmail.com>
2017-01-26 19:18:28 +0800

    fix mangling collision with keep_fnames (#1431)
    * fix mangling collision with keep_fnames
    fixes #1423
    
    * pass mangle options to figure_out_scope()
    bring command-line in line with minify()

--- test/mocha/cli.js
+++ test/mocha/cli.js
@@ -55,7 +55,7 @@ describe("bin/uglifyjs", function () {
        exec(command, function (err, stdout) {
            if (err) throw err;
 
-           assert.strictEqual(stdout, "var bar=function(){function foo(bar){return bar}return foo}();\n" + 
+           assert.strictEqual(stdout, "var bar=function(){function foo(bar){return bar}return foo}();\n" +
                "//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3QvaW5wdXQvaXNzdWUtMTMyMy9zYW1wbGUuanMiXSwibmFtZXMiOlsiYmFyIiwiZm9vIl0sIm1hcHBpbmdzIjoiQUFBQSxHQUFJQSxLQUFNLFdBQ04sUUFBU0MsS0FBS0QsS0FDVixNQUFPQSxLQUdYLE1BQU9DIn0=\n");
            done();
        });
@@ -69,5 +69,35 @@ describe("bin/uglifyjs", function () {
             assert.strictEqual(stdout, "var bar=function(){function foo(bar){return bar}return foo}();\n");
             done();
         });
+    });
+    it("Should work with --keep-fnames (mangle only)", function (done) {
+       var command = uglifyjscmd + ' test/input/issue-1431/sample.js --keep-fnames -m';
+
+       exec(command, function (err, stdout) {
+           if (err) throw err;
+
+           assert.strictEqual(stdout, "function f(r){return function(){function n(n){return n*n}return r(n)}}function g(n){return n(1)+n(2)}console.log(f(g)()==5);\n");
+           done();
+       });
+    });
+    it("Should work with --keep-fnames (mangle & compress)", function (done) {
+       var command = uglifyjscmd + ' test/input/issue-1431/sample.js --keep-fnames -m -c';
+
+       exec(command, function (err, stdout) {
+           if (err) throw err;
+
+           assert.strictEqual(stdout, "function f(r){return function(){function n(n){return n*n}return r(n)}}function g(n){return n(1)+n(2)}console.log(5==f(g)());\n");
+           done();
+       });
+    });
+    it("Should work with keep_fnames under mangler options", function (done) {
+       var command = uglifyjscmd + ' test/input/issue-1431/sample.js -m keep_fnames=true';
+
+       exec(command, function (err, stdout) {
+           if (err) throw err;
+
+           assert.strictEqual(stdout, "function f(r){return function(){function n(n){return n*n}return r(n)}}function g(n){return n(1)+n(2)}console.log(f(g)()==5);\n");
+           done();
+       });
     });
 });
