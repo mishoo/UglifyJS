37ea9df24fe7ada0b9bf08400d1eb29e2019c16d
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-02-16 13:55:24 +0100

    Implement createAccessor using embed_tokens. Adjusted test to match expected result.

--- test/mocha/accessorTokens-1492.js
+++ test/mocha/accessorTokens-1492.js
@@ -3,19 +3,14 @@ var assert = require("assert");
 
 describe("Accessor tokens", function() {
     it("Should fill the token information for accessors (issue #1492)", function() {
+        // location               0         1         2         3         4
+        //                        01234567890123456789012345678901234567890123456789
         var ast = UglifyJS.parse("var obj = { get latest() { return undefined; } }");
-
-        /* a possible way to test, but walking through the whole tree seems more robust against possible AST changes
-        var accessor = ast.body["0"].definitions["0"].value.properties["0"].value;
-        assert(accessor instanceof UglifyJS.AST_Accessor);
-        assert(accessor.start !== undefined);
-        assert(accessor.end !== undefined);
-        */
 
         // test there are no nodes without tokens
         var checkWalker = new UglifyJS.TreeWalker(function(node, descend) {
             if (node instanceof UglifyJS.AST_Accessor) {
-                assert.equal(node.start.pos, 12);
+                assert.equal(node.start.pos, 22);
                 assert.equal(node.end.endpos, 46);
             }
         });
