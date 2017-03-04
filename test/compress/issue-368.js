bcadf56f74e164a47cbf4c4229213db8c90c8b0e
alexlamsl <alexlamsl@gmail.com>
2017-03-05 05:42:15 +0800

    collapse assignment with adjacent subsequent usage - consolidate `cascade` optimisations - support ++/-- postfixes - remove redundant optimisation identified in #1460
    fixes #368

---
+++ test/compress/issue-368.js
@@ -0,0 +1,41 @@
+collapse: {
+    options = {
+        cascade: true,
+        sequences: true,
+        side_effects: true,
+        unused: true,
+    }
+    input: {
+        function f1() {
+            var a;
+            a = typeof b === 'function' ? b() : b;
+            return a !== undefined && c();
+        }
+        function f2(b) {
+            var a;
+            b = c();
+            a = typeof b === 'function' ? b() : b;
+            return 'stirng' == typeof a && d();
+        }
+        function f3(c) {
+            var a;
+            a = b(a / 2);
+            if (a < 0) {
+                c++;
+                return c / 2;
+            }
+        }
+    }
+    expect: {
+        function f1() {
+            return void 0 !== ('function' === typeof b ? b() : b) && c();
+        }
+        function f2(b) {
+            return b = c(), 'stirng' == typeof ('function' === typeof b ? b() : b) && d();
+        }
+        function f3(c) {
+            var a;
+            if ((a = b(a / 2)) < 0) return c++ / 2;
+        }
+    }
+}
