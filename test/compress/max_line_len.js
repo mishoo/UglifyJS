be80f7e706cd6eb1c5f06e433804fda589a8968a
Alex Lam S.L <alexlamsl@gmail.com>
2017-03-10 11:27:30 +0800

    support multi-line string in tests (#1590)
    `expect_exact` sometimes have multiple lines and `\n` are hard to read.
    
    Use array of strings to emulate line breaks and improve readability.

--- test/compress/max_line_len.js
+++ test/compress/max_line_len.js
@@ -7,7 +7,13 @@ too_short: {
             return { c: 42, d: a(), e: "foo"};
         }
     }
-    expect_exact: 'function f(a){\nreturn{\nc:42,\nd:a(),\ne:"foo"}}'
+    expect_exact: [
+        'function f(a){',
+        'return{',
+        'c:42,',
+        'd:a(),',
+        'e:"foo"}}',
+    ]
     expect_warnings: [
         "WARN: Output exceeds 10 characters"
     ]
@@ -22,7 +28,12 @@ just_enough: {
             return { c: 42, d: a(), e: "foo"};
         }
     }
-    expect_exact: 'function f(a){\nreturn{c:42,\nd:a(),e:"foo"}\n}'
+    expect_exact: [
+        'function f(a){',
+        'return{c:42,',
+        'd:a(),e:"foo"}',
+        '}',
+    ]
     expect_warnings: [
     ]
 }
