121d3be4d9ba0dd21a309adbb1737de48f0856e8
alexlamsl <alexlamsl@gmail.com>
2017-04-01 16:58:01 +0800

    fix switch branch elimination Merge unreachable case body with previous fallthrough case
    fixes #1750

---
+++ test/compress/issue-1750.js
@@ -0,0 +1,25 @@
+case_1: {
+    options = {
+        dead_code: true,
+        evaluate: true,
+    }
+    input: {
+        var a = 0, b = 1;
+        switch (true) {
+          case a, true:
+          default:
+            b = 2;
+          case true:
+        }
+        console.log(a, b);
+    }
+    expect: {
+        var a = 0, b = 1;
+        switch (true) {
+          case a, true:
+            b = 2;
+        }
+        console.log(a, b);
+    }
+    expect_stdout: "0 2"
+}
