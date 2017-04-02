9469c03ac976b3cfc1e52abf034ce76f1d6aca59
Alex Lam S.L <alexlamsl@gmail.com>
2017-04-02 17:07:20 +0800

    fix corner case in `switch` (#1765)

--- test/compress/issue-1750.js
+++ test/compress/issue-1750.js
@@ -24,3 +24,31 @@ case_1: {
     }
     expect_stdout: "0 2"
 }
+
+case_2: {
+    options = {
+        dead_code: true,
+        evaluate: true,
+        switches: true,
+    }
+    input: {
+        var a = 0, b = 1;
+        switch (0) {
+          default:
+            b = 2;
+          case a:
+            a = 3;
+          case 0:
+        }
+        console.log(a, b);
+    }
+    expect: {
+        var a = 0, b = 1;
+        switch (0) {
+          case a:
+            a = 3;
+        }
+        console.log(a, b);
+    }
+    expect_stdout: "3 1"
+}
