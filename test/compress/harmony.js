603d92effc92db460efb3d556f73b9a4421f4ec3
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2017-04-03 18:46:05 +0200

    Allow AST_DefaultAssign as parameter (#1778)
    This may be the case for parsing arrow functions,
    as left side expressions are converted to destructuring patterns
    before being converted to parameters.

--- test/compress/harmony.js
+++ test/compress/harmony.js
@@ -314,13 +314,25 @@ default_assign: {
         function f(a, b = 3) {
             console.log(a);
         }
+
         g = ([[] = 123]) => {};
+        h = ([[x, y, z] = [4, 5, 6]] = []) => {};
+
+        function i([[x, y, z] = [4, 5, 6]] = []) {
+            console.log(b);
+        };
     }
     expect: {
         function f(a) {
             console.log(a);
         }
+
         g = ([[] = 123]) => {};
+        h = ([[x, y, z] = [4, 5, 6]] = []) => {};
+
+        function i([[x, y, z] = [4, 5, 6]] = []) {
+            console.log(b);
+        };
     }
 }
 
