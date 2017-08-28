ea9b2fc1fec8c4c923fd5215ebfd59698fb6f926
Shawn Ridenour <zartog@hotmail.com>
2017-08-28 08:00:14 -0600

    Export parse, OutputStream like previous UglifyJS

--- tools/exports.js
+++ tools/exports.js
@@ -3,3 +3,5 @@ exports["TreeWalker"] = TreeWalker;
 exports["TreeTransformer"] = TreeTransformer;
 exports["minify"] = minify;
 exports["_push_uniq"] = push_uniq;
+exports["parse"] = parse;
+exports["OutputStream"] = OutputStream;
