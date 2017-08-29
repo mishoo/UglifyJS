bec5a5daf5973e36358839e68c49a9cf22d2c29b
kzc <zaxxon2011@gmail.com>
2017-08-29 18:19:26 -0400

    avoid generating ternary with spread

--- test/compress/expansions.js
+++ test/compress/expansions.js
@@ -26,3 +26,46 @@ expand_parameters: {
     expect_exact: "(function(a,...b){});(function(...args){});"
 }
 
+avoid_spread_in_ternary: {
+    options = {
+        comparisons: true,
+        conditionals: true,
+        evaluate: true,
+    }
+    input: {
+        function print(...x) {
+            console.log(...x);
+        }
+        var a = [1, 2], b = [3, 4];
+
+        if (Math)
+            print(a);
+        else
+            print(b);
+
+        if (Math)
+            print(...a);
+        else
+            print(b);
+
+        if (Math.no_such_property)
+            print(a);
+        else
+            print(...b);
+    }
+    expect: {
+        function print(...x) {
+            console.log(...x);
+        }
+        var a = [ 1, 2 ], b = [ 3, 4 ];
+        print(Math ? a : b);
+        Math ? print(...a) : print(b);
+        Math.no_such_property ? print(a) : print(...b);
+    }
+    expect_stdout: [
+        "[ 1, 2 ]",
+        "1 2",
+        "3 4",
+    ]
+    node_version: ">=6"
+}
