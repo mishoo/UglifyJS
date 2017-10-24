cf75ccbce97921edab2cec9478f767b96d2cffa1
alexlamsl <alexlamsl@gmail.com>
2017-10-25 00:44:37 +0800

    fix unknown property access

--- test/compress/hoist_props.js
+++ test/compress/hoist_props.js
@@ -98,7 +98,7 @@ issue_2377_3: {
     expect_stdout: "1 27"
 }
 
-direct_access: {
+direct_access_1: {
     options = {
         reduce_vars: true,
         hoist_props: true,
@@ -127,6 +127,55 @@ direct_access: {
         console.log(a, o.a);
     }
     expect_stdout: "2 1"
+}
+
+direct_access_2: {
+    options = {
+        reduce_vars: true,
+        hoist_props: true,
+        toplevel: true,
+        unused: true,
+    }
+    mangle = {
+        toplevel: true,
+    }
+    input: {
+        var o = { a: 1 };
+        var f = function(k) {
+            if (o[k]) return "PASS";
+        };
+        console.log(f("a"));
+    }
+    expect: {
+        var n = { a: 1 };
+        console.log(function(o) {
+            if (n[o]) return "PASS";
+        }("a"));
+    }
+    expect_stdout: "PASS"
+}
+
+direct_access_3: {
+    options = {
+        reduce_vars: true,
+        hoist_props: true,
+        toplevel: true,
+        unused: true,
+    }
+    mangle = {
+        toplevel: true,
+    }
+    input: {
+        var o = { a: 1 };
+        o.b;
+        console.log(o.a);
+    }
+    expect: {
+        var a = { a: 1 };
+        a.b;
+        console.log(a.a);
+    }
+    expect_stdout: "1"
 }
 
 single_use: {
