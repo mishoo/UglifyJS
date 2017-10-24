1282206db3cf915bb223ef25df68b6c14bba5db7
alexlamsl <alexlamsl@gmail.com>
2017-10-25 01:53:18 +0800

    implement name generation & collision avoidance

--- test/compress/hoist_props.js
+++ test/compress/hoist_props.js
@@ -7,9 +7,6 @@ issue_2377_1: {
         toplevel: true,
         unused: true,
     }
-    mangle = {
-        toplevel: true,
-    }
     input: {
         var obj = {
             foo: 1,
@@ -24,10 +21,10 @@ issue_2377_1: {
         console.log(obj.foo, obj.cube(3));
     }
     expect: {
-        var n = 1, o = function(n) {
-            return n * n * n;
+        var obj_foo = 1, obj_cube = function(x) {
+            return x * x * x;
         };
-        console.log(n, o(3));
+        console.log(obj_foo, obj_cube(3));
     }
     expect_stdout: "1 27"
 }
@@ -42,9 +39,6 @@ issue_2377_2: {
         toplevel: true,
         unused: true,
     }
-    mangle = {
-        toplevel: true,
-    }
     input: {
         var obj = {
             foo: 1,
@@ -59,8 +53,8 @@ issue_2377_2: {
         console.log(obj.foo, obj.cube(3));
     }
     expect: {
-        console.log(1, function(n) {
-            return n * n * n;
+        console.log(1, function(x) {
+            return x * x * x;
         }(3));
     }
     expect_stdout: "1 27"
@@ -75,9 +69,6 @@ issue_2377_3: {
         reduce_vars: true,
         toplevel: true,
         unused: true,
-    }
-    mangle = {
-        toplevel: true,
     }
     input: {
         var obj = {
@@ -105,9 +96,6 @@ direct_access_1: {
         toplevel: true,
         unused: true,
     }
-    mangle = {
-        toplevel: true,
-    }
     input: {
         var a = 0;
         var obj = {
@@ -119,12 +107,12 @@ direct_access_1: {
     }
     expect: {
         var a = 0;
-        var o = {
+        var obj = {
             a: 1,
             b: 2,
         };
-        for (var r in o) a++;
-        console.log(a, o.a);
+        for (var k in obj) a++;
+        console.log(a, obj.a);
     }
     expect_stdout: "2 1"
 }
@@ -136,9 +124,6 @@ direct_access_2: {
         toplevel: true,
         unused: true,
     }
-    mangle = {
-        toplevel: true,
-    }
     input: {
         var o = { a: 1 };
         var f = function(k) {
@@ -147,9 +132,9 @@ direct_access_2: {
         console.log(f("a"));
     }
     expect: {
-        var n = { a: 1 };
-        console.log(function(o) {
-            if (n[o]) return "PASS";
+        var o = { a: 1 };
+        console.log(function(k) {
+            if (o[k]) return "PASS";
         }("a"));
     }
     expect_stdout: "PASS"
@@ -162,18 +147,15 @@ direct_access_3: {
         toplevel: true,
         unused: true,
     }
-    mangle = {
-        toplevel: true,
-    }
     input: {
         var o = { a: 1 };
         o.b;
         console.log(o.a);
     }
     expect: {
-        var a = { a: 1 };
-        a.b;
-        console.log(a.a);
+        var o = { a: 1 };
+        o.b;
+        console.log(o.a);
     }
     expect_stdout: "1"
 }
@@ -184,9 +166,6 @@ single_use: {
         hoist_props: true,
         toplevel: true,
         unused: true,
-    }
-    mangle = {
-        toplevel: true,
     }
     input: {
         var obj = {
@@ -203,4 +182,41 @@ single_use: {
             },
         }.bar());
     }
+}
+
+name_collision: {
+    options = {
+        reduce_vars: true,
+        hoist_props: true,
+        toplevel: true,
+    }
+    input: {
+        var obj_foo = 1;
+        var obj_bar = 2;
+        function f() {
+            var obj = {
+                foo: 3,
+                bar: 4,
+                "b-r": 5,
+                "b+r": 6,
+                "b!r": 7,
+            };
+            console.log(obj_foo, obj.foo, obj.bar, obj["b-r"], obj["b+r"], obj["b!r"]);
+        }
+        f();
+    }
+    expect: {
+        var obj_foo = 1;
+        var obj_bar = 2;
+        function f() {
+            var obj_foo$0 = 3,
+                obj_bar = 4,
+                obj_b_r = 5,
+                obj_b_r$0 = 6,
+                obj_b_r$1 = 7;
+            console.log(obj_foo, obj_foo$0, obj_bar, obj_b_r, obj_b_r$0, obj_b_r$1);
+        }
+        f();
+    }
+    expect_stdout: "1 3 4 5 6 7"
 }
