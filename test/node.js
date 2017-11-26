b2222bd2d71dc9d963c2d5b85849b775ad848d4e
yangweixian <yangweixian@kuaikanmanhua.com>
2017-11-26 23:14:26 +0800

    add test

--- test/node.js
+++ test/node.js
@@ -1,6 +1,10 @@
 var fs = require("fs");
 
-new Function("MOZ_SourceMap", "exports", require("../tools/node").FILES.map(function(file) {
-    if (/exports\.js$/.test(file)) file = require.resolve("./exports");
-    return fs.readFileSync(file, "utf8");
-}).join("\n\n"))(require("source-map"), exports);
+new Function("MOZ_SourceMap", "exports", function() {
+    var code = require("../tools/node").FILES.map(function(file) {
+        if (/exports\.js$/.test(file)) file = require.resolve("./exports");
+        return fs.readFileSync(file, "utf8");
+    });
+    code.push(";var domprops = " + JSON.stringify(require("../tools/domprops.json")));
+    return code.join("\n\n");
+}())(require("source-map"), exports);
