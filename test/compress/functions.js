b12379d465621a3297a6459fbcffa89bee5eebed
alexlamsl <alexlamsl@gmail.com>
2017-12-18 14:07:21 +0800

    ensure order of execution

--- test/compress/functions.js
+++ test/compress/functions.js
@@ -940,12 +940,18 @@ unsafe_apply_1: {
         (function(a, b) {
             console.log(this, a, b);
         }).apply("foo", [ "bar" ]);
+        (function(a, b) {
+            console.log(a, b);
+        }).apply("foo", [ "bar" ], "baz");
     }
     expect: {
         console.log("bar", void 0);
         (function(a, b) {
             console.log(this, a, b);
         }).call("foo", "bar");
+        (function(a, b) {
+            console.log(a, b);
+        }).apply("foo", [ "bar" ], "baz");
     }
     expect_stdout: true
 }
