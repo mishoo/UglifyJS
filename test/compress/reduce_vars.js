1467126f2af0000511edd02402f91c6c7b465f09
alexlamsl <alexlamsl@gmail.com>
2018-01-03 17:53:17 +0800

    drop `unused` assignment based on `reduce_vars`

--- test/compress/reduce_vars.js
+++ test/compress/reduce_vars.js
@@ -299,7 +299,7 @@ unsafe_evaluate_modified: {
         console.log(function(){ var o={p:1}; o.p++; console.log(o.p); return o.p; }());
         console.log(function(){ var o={p:2}; --o.p; console.log(o.p); return o.p; }());
         console.log(function(){ var o={p:3}; o.p += ""; console.log(o.p); return o.p; }());
-        console.log(function(){ var o={p:4}; o = {}; console.log(o.p); return o.p; }());
+        console.log(function(){ var o; o = {}; console.log(o.p); return o.p; }());
         console.log(function(){ var o={p:5}; o.p = -9; console.log(o.p); return o.p; }());
         function inc() { this.p++; }
         console.log(function(){ var o={p:6}; inc.call(o); console.log(o.p); return o.p; }());
