bc01a85ba0d0aa681958e1e9607e4fecda2b2302
Alex Lam S.L <alexlamsl@gmail.com>
2018-01-22 14:57:23 +0800

    add parenthesis around sequence in `yield` (#2833)
    fixes #2832

--- test/compress/yield.js
+++ test/compress/yield.js
@@ -211,3 +211,43 @@ issue_2689: {
     }
     expect_exact: "function*y(){return new(yield x())}"
 }
+
+issue_2832: {
+    beautify = {
+        beautify: true,
+    }
+    input: {
+        function* gen(i) {
+            const result = yield (x = i, -x);
+            var x;
+            console.log(x);
+            console.log(result);
+            yield 2;
+        }
+        var x = gen(1);
+        console.log(x.next("first").value);
+        console.log(x.next("second").value);
+    }
+    expect_exact: [
+        "function* gen(i) {",
+        "    const result = yield (x = i, -x);",
+        "    var x;",
+        "    console.log(x);",
+        "    console.log(result);",
+        "    yield 2;",
+        "}",
+        "",
+        "var x = gen(1);",
+        "",
+        'console.log(x.next("first").value);',
+        "",
+        'console.log(x.next("second").value);',
+    ]
+    expect_stdout: [
+        "-1",
+        "1",
+        "second",
+        "2",
+    ]
+    node_version: ">=4"
+}
