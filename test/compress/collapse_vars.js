4d1d205803177a1710381096ea5150c6fda77fdb
alexlamsl <alexlamsl@gmail.com>
2018-02-14 02:45:24 +0800

    fix `collapse_vars` within loops
    fixes #2914

--- test/compress/collapse_vars.js
+++ test/compress/collapse_vars.js
@@ -4546,3 +4546,76 @@ issue_2908: {
     }
     expect_stdout: "2"
 }
+
+issue_2914_1: {
+    options = {
+        collapse_vars: true,
+    }
+    input: {
+        function read(input) {
+            var i = 0;
+            var e = 0;
+            var t = 0;
+            while (e < 32) {
+                var n = input[i++];
+                t |= (127 & n) << e;
+                if (0 === (128 & n))
+                    return t;
+                e += 7;
+            }
+        }
+        console.log(read([129]));
+    }
+    expect: {
+        function read(input) {
+            var i = 0;
+            var e = 0;
+            var t = 0;
+            while (e < 32) {
+                var n = input[i++];
+                t |= (127 & n) << e;
+                if (0 === (128 & n))
+                    return t;
+                e += 7;
+            }
+        }
+        console.log(read([129]));
+    }
+    expect_stdout: "1"
+}
+
+issue_2914_2: {
+    options = {
+        collapse_vars: true,
+    }
+    input: {
+        function read(input) {
+            var i = 0;
+            var e = 0;
+            var t = 0;
+            while (e < 32) {
+                var n = input[i++];
+                t = (127 & n) << e;
+                if (0 === (128 & n))
+                    return t;
+                e += 7;
+            }
+        }
+        console.log(read([129]));
+    }
+    expect: {
+        function read(input) {
+            var i = 0;
+            var e = 0;
+            var t = 0;
+            while (e < 32) {
+                var n = input[i++];
+                if (0 === (128 & n))
+                    return t = (127 & n) << e;
+                e += 7;
+            }
+        }
+        console.log(read([129]));
+    }
+    expect_stdout: "0"
+}
