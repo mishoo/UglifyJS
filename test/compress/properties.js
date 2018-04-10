90199d0a961bb75cc5bf6d8ade8bf5b0098cb341
Alex Lam S.L <alexlamsl@gmail.com>
2018-04-11 01:35:42 +0800

    extend `join_vars` on object assignments (#3072)

--- test/compress/properties.js
+++ test/compress/properties.js
@@ -1208,6 +1208,37 @@ join_object_assignments_3: {
     expect_stdout: "PASS"
 }
 
+join_object_assignments_4: {
+    options = {
+        join_vars: true,
+        sequences: true,
+    }
+    input: {
+        var o;
+        console.log(o);
+        o = {};
+        o.a = "foo";
+        console.log(o.b);
+        o.b = "bar";
+        console.log(o.a);
+    }
+    expect: {
+        var o;
+        console.log(o),
+        o = {
+            a: "foo",
+        },
+        console.log(o.b),
+        o.b = "bar",
+        console.log(o.a);
+    }
+    expect_stdout: [
+        "undefined",
+        "undefined",
+        "foo",
+    ]
+}
+
 join_object_assignments_return_1: {
     options = {
         join_vars: true,
