22cea023d1f1f54eaba036ddfd63800f3fa50be6
Alex Lam S.L <alexlamsl@gmail.com>
2018-04-28 02:47:49 +0800

    improve numeral compression (#3108)

--- test/compress/numbers.js
+++ test/compress/numbers.js
@@ -1,21 +1,49 @@
 hex_numbers_in_parentheses_for_prototype_functions: {
-    input: {
-        (-2);
-        (-2).toFixed(0);
-
-        (2);
-        (2).toFixed(0);
-
-        (0.2);
-        (0.2).toFixed(0);
-
-        (0.00000002);
-        (0.00000002).toFixed(0);
-
-        (1000000000000000128);
-        (1000000000000000128).toFixed(0);
+    beautify = {
+        beautify: true,
     }
-    expect_exact: "-2;(-2).toFixed(0);2;2..toFixed(0);.2;.2.toFixed(0);2e-8;2e-8.toFixed(0);0xde0b6b3a7640080;(0xde0b6b3a7640080).toFixed(0);"
+    input: {
+        function f() {
+            (-2);
+            (-2).toFixed(0);
+
+            (2);
+            (2).toFixed(0);
+
+            (0.2);
+            (0.2).toFixed(0);
+
+            (2.34e20);
+            (2.34e20).toFixed(0);
+
+            (0.00000002);
+            (0.00000002).toFixed(0);
+
+            (1000000000000000128);
+            (1000000000000000128).toFixed(0);
+
+            (-1000000000000000128);
+            (-1000000000000000128).toFixed(0);
+        }
+    }
+    expect_exact: [
+        "function f() {",
+        "    -2;",
+        "    (-2).toFixed(0);",
+        "    2;",
+        "    2..toFixed(0);",
+        "    .2;",
+        "    .2.toFixed(0);",
+        "    234e18;",
+        "    234e18.toFixed(0);",
+        "    2e-8;",
+        "    2e-8.toFixed(0);",
+        "    0xde0b6b3a7640080;",
+        "    (0xde0b6b3a7640080).toFixed(0);",
+        "    -0xde0b6b3a7640080;",
+        "    (-0xde0b6b3a7640080).toFixed(0);",
+        "}",
+    ]
 }
 
 comparisons: {
