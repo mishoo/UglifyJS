fb7359d31138fc829d080908a76fbcf39473e101
alexlamsl <alexlamsl@gmail.com>
2018-05-10 05:06:24 +0800

    fix corner case in call binding
    fixes #3127

--- test/compress/issue-973.js
+++ test/compress/issue-973.js
@@ -3,8 +3,9 @@ this_binding_conditionals: {
         conditionals: true,
         evaluate: true,
         side_effects: true,
-    };
+    }
     input: {
+        "use strict";
         (1 && a)();
         (0 || a)();
         (0 || 1 && a)();
@@ -26,6 +27,7 @@ this_binding_conditionals: {
         (1 ? eval : 0)();
     }
     expect: {
+        "use strict";
         a();
         a();
         a();
@@ -53,13 +55,15 @@ this_binding_collapse_vars: {
         collapse_vars: true,
         toplevel: true,
         unused: true,
-    };
+    }
     input: {
+        "use strict";
         var c = a; c();
         var d = a.b; d();
         var e = eval; e();
     }
     expect: {
+        "use strict";
         a();
         (0, a.b)();
         (0, eval)();
@@ -69,31 +73,88 @@ this_binding_collapse_vars: {
 this_binding_side_effects: {
     options = {
         side_effects : true
-    };
+    }
     input: {
-        (function (foo) {
+        (function(foo) {
             (0, foo)();
             (0, foo.bar)();
-            (0, eval)('console.log(foo);');
+            (0, eval)("console.log(foo);");
         }());
-        (function (foo) {
+        (function(foo) {
+            "use strict";
+            (0, foo)();
+            (0, foo.bar)();
+            (0, eval)("console.log(foo);");
+        }());
+        (function(foo) {
             var eval = console;
             (0, foo)();
             (0, foo.bar)();
-            (0, eval)('console.log(foo);');
+            (0, eval)("console.log(foo);");
         }());
     }
     expect: {
-        (function (foo) {
+        (function(foo) {
             foo();
             (0, foo.bar)();
-            (0, eval)('console.log(foo);');
+            eval("console.log(foo);");
         }());
-        (function (foo) {
+        (function(foo) {
+            "use strict";
+            foo();
+            (0, foo.bar)();
+            (0, eval)("console.log(foo);");
+        }());
+        (function(foo) {
             var eval = console;
             foo();
             (0, foo.bar)();
-            (0, eval)('console.log(foo);');
+            eval("console.log(foo);");
         }());
     }
+}
+
+this_binding_sequences: {
+    options = {
+        sequences: true,
+        side_effects: true,
+    }
+    input: {
+        console.log(typeof function() {
+            return eval("this");
+        }());
+        console.log(typeof function() {
+            "use strict";
+            return eval("this");
+        }());
+        console.log(typeof function() {
+            return (0, eval)("this");
+        }());
+        console.log(typeof function() {
+            "use strict";
+            return (0, eval)("this");
+        }());
+    }
+    expect: {
+        console.log(typeof function() {
+            return eval("this");
+        }()),
+        console.log(typeof function() {
+            "use strict";
+            return eval("this");
+        }()),
+        console.log(typeof function() {
+            return eval("this");
+        }()),
+        console.log(typeof function() {
+            "use strict";
+            return (0, eval)("this");
+        }());
+    }
+    expect_stdout: [
+        "object",
+        "undefined",
+        "object",
+        "object",
+    ]
 }
