cfebeb2f63929a9e1a73a606ff2bc9a75e380b28
Alex Lam S.L <alexlamsl@gmail.com>
2018-08-09 17:34:28 +0800

    fix corner case in `mangle` workaround for Safari (#3230)
    fixes #3227

--- test/compress/webkit.js
+++ test/compress/webkit.js
@@ -58,3 +58,52 @@ lambda_name_mangle: {
     expect_exact: "console.log(typeof function o(n){});"
     expect_stdout: "function"
 }
+
+lambda_name_mangle_ie8: {
+    mangle = {
+        ie8: true,
+        toplevel: true,
+    }
+    input: {
+        console.log(typeof function foo(bar) {});
+    }
+    expect_exact: "console.log(typeof function n(o){});"
+    expect_stdout: "function"
+}
+
+function_name_mangle: {
+    options = {
+        keep_fnames: true,
+        reduce_vars: true,
+        unused: true,
+    }
+    mangle = {}
+    input: {
+        (function() {
+            function foo(bar) {}
+            console.log(typeof foo);
+        })();
+    }
+    expect_exact: "(function(){console.log(typeof function o(n){})})();"
+    expect_stdout: "function"
+}
+
+function_name_mangle_ie8: {
+    options = {
+        keep_fnames: true,
+        reduce_vars: true,
+        unused: true,
+    }
+    mangle = {
+        ie8: true,
+        toplevel: true,
+    }
+    input: {
+        (function() {
+            function foo(bar) {}
+            console.log(typeof foo);
+        })();
+    }
+    expect_exact: "(function(){console.log(typeof function n(o){})})();"
+    expect_stdout: "function"
+}
