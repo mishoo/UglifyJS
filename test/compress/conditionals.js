e5436ca5665378d67bb228704736d1fe5fa7b6ee
Alex Lam S.L <alexlamsl@gmail.com>
2019-04-25 15:15:50 +0800

    enhance `side_effects` (#3384)

--- test/compress/conditionals.js
+++ test/compress/conditionals.js
@@ -1435,3 +1435,39 @@ iife_condition: {
     }
     expect_stdout: "PASS"
 }
+
+angularjs_chain: {
+    options = {
+        conditionals: true,
+        passes: 2,
+        side_effects: true,
+    }
+    input: {
+        function nonComputedMember(left, right, context, create) {
+            var lhs = left();
+            if (create && create !== 1) {
+                if (lhs && lhs[right] == null) {
+                    lhs[right] = {};
+                }
+            }
+            var value = lhs != null ? lhs[right] : undefined;
+            if (context) {
+                return { context: lhs, name: right, value: value };
+            } else {
+                return value;
+            }
+        }
+    }
+    expect: {
+        function nonComputedMember(left, right, context, create) {
+            var lhs = left();
+            create && 1 !== create && lhs && null == lhs[right] && (lhs[right] = {});
+            var value = null != lhs ? lhs[right] : void 0;
+            return context ? {
+                context: lhs,
+                name: right,
+                value: value
+            } : value;
+        }
+    }
+}
