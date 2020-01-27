16cfce2228f98253fba8bc4a259f7caa2d4f192a
alexlamsl <alexlamsl@gmail.com>
2020-01-28 07:44:22 +0800

    enhance `sequences`

--- test/compress/functions.js
+++ test/compress/functions.js
@@ -2244,7 +2244,7 @@ issue_3076: {
         var c = "PASS";
         (function(b) {
             var n = 2;
-            while (--b + (e = void 0, e && (c = "FAIL"), e = 5, 1).toString() && --n > 0);
+            while (--b + (e = void 0, e && (c = "FAIL"), e = 5, 1..toString()) && --n > 0);
             var e;
         })(2),
         console.log(c);
