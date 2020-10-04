d6eca73a14ac5dcae51faeb87155e19da253c674
alexlamsl <alexlamsl@gmail.com>
2020-10-05 03:11:05 +0800

    retrofit `try-catch-finally` as block-scoped - support optional catch binding

--- test/reduce.js
+++ test/reduce.js
@@ -427,7 +427,7 @@ module.exports = function reduce_test(testcase, minify_options, reduce_options) 
                     if (node.TYPE == "Call" && node.expression.print_to_string() == "console.log") {
                         return to_sequence(node.args);
                     }
-                    if (node instanceof U.AST_Catch) {
+                    if (node instanceof U.AST_Catch && node.argname) {
                         descend(node, this);
                         node.body.unshift(new U.AST_SimpleStatement({
                             body: wrap_with_console_log(new U.AST_SymbolRef(node.argname)),
