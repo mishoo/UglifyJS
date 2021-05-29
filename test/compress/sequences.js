260431f4e00be191a9c5db191c66e552898b0708
Alex Lam S.L <alexlamsl@gmail.com>
2021-05-29 05:48:34 +0100

    improve handling of lexical scope (#4979)

--- test/compress/sequences.js
+++ test/compress/sequences.js
@@ -727,8 +727,7 @@ side_effects_cascade_1: {
     }
     expect: {
         function f(a, b) {
-            (a -= 42) < 0 && (a = 0),
-            b.a = a;
+            b.a = a = (a -= 42) < 0 ? 0 : a;
         }
         var m = {}, n = {};
         f(13, m),
