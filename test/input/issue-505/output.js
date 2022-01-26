0a273e9f75296742334f002b1f3d2c2d3fc03d3e
alexlamsl <alexlamsl@gmail.com>
2022-01-26 11:45:23 +0800

    fix corner case in `max_line_len` - speed up `max_line_len` & `preserve_line`

--- test/input/issue-505/output.js
+++ test/input/issue-505/output.js
@@ -1,5 +1,5 @@
 function test(a){
-"aaaaaaaaaaaaaaaa"
-;a(err,data),a(err,
+"aaaaaaaaaaaaaaaa";
+a(err,data),a(err,
 data)}
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIjAiXSwibmFtZXMiOlsidGVzdCIsImNhbGxiYWNrIiwiZXJyIiwiZGF0YSJdLCJtYXBwaW5ncyI6IkFBQUEsU0FBU0EsS0FBS0M7O0NBRVZBLEVBQVNDLElBQUtDLE1BQ2RGLEVBQVNDO0FBQUtDIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIjAiXSwibmFtZXMiOlsidGVzdCIsImNhbGxiYWNrIiwiZXJyIiwiZGF0YSJdLCJtYXBwaW5ncyI6IkFBQUEsU0FBU0E7SEFBS0M7QUFFVkE7aEJBQVNDLElBQUtDLE1BQ2RGLEVBQVNDLElBQUtDIn0=
