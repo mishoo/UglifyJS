9f1c2059486e6855799f2efa712b56ab8cb94be0
alexlamsl <alexlamsl@gmail.com>
2022-02-10 14:24:53 +0800

    patch `export default` within sandbox correctly
    fixes #5345

---
+++ test/input/reduce/export_default.js
@@ -0,0 +1,17 @@
+var unused;
+export default class {
+    ____11111() {
+        a, b, c, d, e;
+        f, g, h, i, j;
+        k, l, m, n, o;
+        p, q, r, s, t;
+        u, v, w, x, y, z;
+        A, B, C, D, E;
+        F, G, H, I, J;
+        K, L, M, N, O;
+        P, Q, R, S, T;
+        U, V, W, X, Y, Z;
+        $, _;
+        unused;
+    }
+}
