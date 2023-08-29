13cc2b5be84e23b4bb178718342783f80ea7089a
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-08-30 03:18:34 +0530

    Update node.js

--- tools/node.js
+++ tools/node.js
@@ -1,4 +1,4 @@
-var fs = require("fs");
+let fs = require("fs");
 
 exports.FILES = [
     require.resolve("../lib/utils.js"),
@@ -16,7 +16,7 @@ exports.FILES = [
 ];
 
 new Function("domprops", "exports", function() {
-    var code = exports.FILES.map(function(file) {
+    let code = exports.FILES.map(function(file) {
         return fs.readFileSync(file, "utf8");
     });
     code.push("exports.describe_ast = " + describe_ast.toString());
@@ -32,7 +32,7 @@ function to_comment(value) {
 
 if (+process.env["UGLIFY_BUG_REPORT"]) exports.minify = function(files, options) {
     if (typeof options == "undefined") options = "<<undefined>>";
-    var code = [
+    let code = [
         "// UGLIFY_BUG_REPORT",
         to_comment(options),
     ];
@@ -40,7 +40,7 @@ if (+process.env["UGLIFY_BUG_REPORT"]) exports.minify = function(files, options)
         code.push("");
         code.push("//-------------------------------------------------------------")
         code.push("// INPUT CODE", files);
-    } else for (var name in files) {
+    } else for (let name in files) {
         code.push("");
         code.push("//-------------------------------------------------------------")
         code.push(to_comment(name), files[name]);
@@ -49,19 +49,19 @@ if (+process.env["UGLIFY_BUG_REPORT"]) exports.minify = function(files, options)
         code.push("");
         code.push("//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9");
     }
-    var result = { code: code.join("\n") };
+    let result = { code: code.join("\n") };
     if (options.sourceMap) result.map = '{"version":3,"sources":[],"names":[],"mappings":""}';
     return result;
 };
 
 function describe_ast() {
-    var out = OutputStream({ beautify: true });
+    let out = OutputStream({ beautify: true });
     doitem(AST_Node);
     return out.get() + "\n";
 
     function doitem(ctor) {
         out.print("AST_" + ctor.TYPE);
-        var props = ctor.SELF_PROPS.filter(function(prop) {
+        let props = ctor.SELF_PROPS.filter(function(prop) {
             return !/^\$/.test(prop);
         });
         if (props.length > 0) {
@@ -93,14 +93,14 @@ function describe_ast() {
 }
 
 function infer_options(options) {
-    var result = exports.minify("", options);
+    let result = exports.minify("", options);
     return result.error && result.error.defs;
 }
 
 exports.default_options = function() {
-    var defs = infer_options({ 0: 0 });
+    let defs = infer_options({ 0: 0 });
     Object.keys(defs).forEach(function(component) {
-        var options = { module: false };
+        let options = { module: false };
         options[component] = { 0: 0 };
         if (options = infer_options(options)) {
             defs[component] = options;
