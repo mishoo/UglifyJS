9ab3061f1d7d04e6b99996d7db1b91a949c2a3c7
alexlamsl <alexlamsl@gmail.com>
2024-06-08 23:38:30 +0300

    workaround `function` declaration quirks in ES6+
    fixes #1666

--- test/compress/blocks.js
+++ test/compress/blocks.js
@@ -47,3 +47,45 @@ keep_some_blocks: {
         } else stuff();
     }
 }
+
+issue_1666: {
+    input: {
+        var a = 42;
+        {
+            function a() {}
+            a();
+        }
+        console.log("PASS");
+    }
+    expect: {
+        var a = 42;
+        {
+            function a() {}
+            a();
+        }
+        console.log("PASS");
+    }
+    expect_stdout: true
+}
+
+issue_1666_strict: {
+    input: {
+        "use strict";
+        var a = 42;
+        {
+            function a() {}
+            a();
+        }
+        console.log("PASS");
+    }
+    expect: {
+        "use strict";
+        var a = 42;
+        {
+            function a() {}
+            a();
+        }
+        console.log("PASS");
+    }
+    expect_stdout: true
+}
